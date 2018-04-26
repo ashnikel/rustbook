d34630ccfe50f3c01a766282fcc69af6b9e6d577
ashNikel <ashnikel@gmail.com>
2018-04-26 16:52:14 +0300

    Common Collections Exercise 2

---
+++ ch08-3-exercise2/src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+
+fn main() {
+    println!("Please input your text.");
+
+    let mut text = String::new();
+    io::stdin().read_line(&mut text)
+        .expect("Failed to read line");
+
+    println!("Original text: {}", text);
+
+    // let consonants = String::from("bcdfghjklmnpqrstvwxz");
+    let vowels = String::from("aeiouy");
+
+    let mut pig_text = String::new();
+
+    for word in text.split_whitespace() {
+        let mut pig_word = String::new();
+        let first_letter = word.chars().next().unwrap();
+        if vowels.contains(first_letter) {
+            pig_word = word.to_string();
+            pig_word.push_str("-hay");
+        } else {
+            let mut chars: Vec<char> = word.chars().collect();
+            let letter = chars.remove(0);
+            chars.push('-');
+            chars.push(letter);
+            chars.push('a');
+            chars.push('y');
+            pig_word = chars.join();
+            // for c in word.chars() {
+            //     println!("{}", c);
+            // }
+        }
+
+        pig_text.push_str(&pig_word);
+        pig_text.push(' ');
+    }
+    println!("Pig text: {}", pig_text);
+}

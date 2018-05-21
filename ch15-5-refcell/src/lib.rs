deb7edad6f0cd97043774354d6f7cc4231995823
ashNikel <ashnikel@gmail.com>
2018-05-21 16:51:52 +0300

    RefCell

---
+++ ch15-5-refcell/src/lib.rs
@@ -0,0 +1,70 @@
+pub trait Messanger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: 'a + Messanger> {
+    messanger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+    where T: Messanger {
+    pub fn new(messanger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messanger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 0.75 && percentage_of_max < 0.9 {
+            self.messanger.send("Warning: You've used up over 75% of your quota!");
+        } else if percentage_of_max >= 0.9 && percentage_of_max < 1.0 {
+            self.messanger.send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 1.0 {
+            self.messanger.send("Error: You are over your quota!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger { sent_messages: RefCell::new(vec![]) }
+        }
+    }
+
+    impl Messanger for MockMessenger {
+        fn send(&self, message: &str) {
+            let mut one_borrow = self.sent_messages.borrow_mut();
+         // let mut two_borrow = self.sent_messages.borrow_mut(); // panic!
+
+            one_borrow.push(String::from(message));
+         // two_borrow.push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messanger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messanger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messanger.sent_messages.borrow().len(), 1);
+    }
+}

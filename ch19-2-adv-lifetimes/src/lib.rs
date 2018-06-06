0a8f21cb9764aaa977bc8baf39267ff6631601f0
ashNikel <ashnikel@gmail.com>
2018-06-06 16:58:04 +0300

    Advanced lifetimes

---
+++ ch19-2-adv-lifetimes/src/lib.rs
@@ -0,0 +1,17 @@
+#![allow(dead_code)]
+
+struct Context<'a>(&'a str);
+
+struct Parser<'a> {
+    context: &'a Context<'a>,
+}
+
+impl<'a> Parser<'a> {
+    fn parse(&self) -> Result<(), &str> {
+        Err(&self.context.0[1..])
+    }
+}
+
+fn parse_context(context: Context) -> Result<(), &str> {
+    Parser { context: &context }.parse()
+}
